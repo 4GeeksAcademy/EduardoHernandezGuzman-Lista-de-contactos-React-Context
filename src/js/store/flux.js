958f5eb3b973e2455c8c4191c8483349cb8ebe67
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-11-23 21:52:06 +0000

    Actualización

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -18,16 +18,16 @@ const getState = ({ getStore, getActions, setStore }) => {
 			// 		initial: "white"
 			// 	}
 			// ],
-				contacts: [
-					{
-						full_name: "",
-						address: "",
-						phone: "",
-						email: ""
-					}
-				
-				]
-			},
+			contacts: [
+				{
+					full_name: "",
+					address: "",
+					phone: "",
+					email: ""
+				}
+
+			]
+		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			// exampleFunction: () => {
@@ -37,10 +37,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 			loadSomeData: () => {
 
-			//Traerme la agenda	
-			
-			
-					
+				//Traerme la agenda	
+
+
+
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
@@ -52,51 +52,78 @@ const getState = ({ getStore, getActions, setStore }) => {
 				var requestOptions = {
 					method: 'GET',
 					redirect: 'follow'
-				  };
-				  fetch("https://playground.4geeks.com/apis/fake/contact/agenda/miagenda", requestOptions)
+				};
+				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/miagenda", requestOptions)
 					.then(response => response.json())
 					.then(data => setStore({ contacts: data }))
-				},
+			},
 
-			
-			
-		
-			
+
+
+
+
 			//Borrar un contacto de la agenda
 			borrarContacto: (indexABorrar) => {
-				
+
 				var requestOptions = {
 					method: 'DELETE',
 					redirect: 'follow'
-				  };
-				  
-				  fetch("https://playground.4geeks.com/apis/fake/contact/" + indexABorrar, requestOptions)
+				};
+
+				fetch("https://playground.4geeks.com/apis/fake/contact/" + indexABorrar, requestOptions)
 					.then(response => response.json())
 					.then(data => setStore({ contacts: data }))
-					setTimeout(() => {location.reload();}, 1000);
-					
-					
-					
-				
-				
+				setTimeout(() => { location.reload(); }, 1000);
+
+
+
+
+
 			},
-			
+
 
 			//Borrar TODOS los contactos de la agenda
 			borrarTodosLosContactos: () => {
-				
+
 				var requestOptions = {
 					method: 'DELETE',
 					redirect: 'follow'
-				  };
-				  
-				  fetch("https://playground.4geeks.com/apis/fake/contact/agenda/miagenda", requestOptions)
+				};
+
+				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/miagenda", requestOptions)
 					.then(response => response.json())
 					.then(data => setStore({ contacts: data }))
-					setTimeout(() => {location.reload();}, 1000);
-				},
+				setTimeout(() => { location.reload(); }, 1000);
+			},
 
-			
+			crearContacto: () => {
+
+				var myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
+
+				var raw = JSON.stringify({
+					"full_name": "Manolo Bradley",
+					"email": "dave@gmail.com",
+					"agenda_slug": "miagenda",
+					"address": "47568 NW 34ST, 33434 FL, USA",
+					"phone": "7864445566"
+				});
+
+				var requestOptions = {
+					method: 'POST',
+					headers: myHeaders,
+					body: raw,
+					redirect: 'follow'
+				};
+
+				fetch("https://playground.4geeks.com/apis/fake/contact/", requestOptions)
+					.then(response => response.json())
+					.then(result => console.log(result))
+					
+
+			},
+
+
 
 
 			// changeColor: (index, color) => {
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,17 +1,75 @@
-import React from "react";
-// import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h3>Nuevo contacto</h3>
+export const Home = () => {
+	const [fullName, setFullName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [address, setAddress] = useState("");
+	const [inputText, setInputText] = useState([]);
 
-		{/* <h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a> */}
-	</div>
-);
+	
+
+	return (
+		<>
+			<div className="mb-3">
+				<label htmlFor="fullName" className="form-label">Full Name</label>
+				<input 
+					onChange={(e)=> setFullName(e.target.value)} 
+					value={fullName}
+					type="text" 
+					className="form-control" 
+					id="fullName" 
+					placeholder="Enter your full name" 
+				/>
+			</div>
+			<div className="mb-3">
+				<label htmlFor="email" className="form-label">Email address</label>
+				<input 
+					onChange={(e)=> setEmail(e.target.value)} 
+					value={email}
+					type="text" 
+					className="form-control" 
+					id="email" 
+					placeholder="Enter your email" 
+				/>
+			</div>
+			<div className="mb-3">
+				<label htmlFor="phone" className="form-label">Phone</label>
+				<input 
+					onChange={(e)=> setPhone(e.target.value)} 
+					value={phone}
+					type="text" 
+					className="form-control" 
+					id="phone" 
+					placeholder="Enter your phone number" 
+				/>
+			</div>
+			<div className="mb-3">
+				<label htmlFor="address" className="form-label">Address</label>
+				<input 
+					onChange={(e)=> setAddress(e.target.value)} 
+					value={address}
+					type="text" 
+					className="form-control" 
+					id="address" 
+					placeholder="Enter your address" 
+				/>
+			</div>
+			<button 
+				onClick={() => {
+					setInputText(inputText.concat({ fullName, email, phone, address }));
+					setFullName("");
+					setEmail("");
+					setPhone("");
+					setAddress("");
+				}} 
+				type="button" 
+				className="btn btn-primary"
+			>
+				Save
+			</button>
+		</>
+	);
+};
+
